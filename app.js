2f3dfb1e43bd61f1b01778920af5bc1ca0566a36
austinloveless <austinloveless5171@gmail.com>
2018-07-19 11:38:19 -0600

    nothing to commit

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8000;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/auth_host_demo");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/secret", (req, res) => {
+  res.render("secret");
+});
+
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
